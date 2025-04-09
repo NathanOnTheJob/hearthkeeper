278cc31b1746ebb55c7aed165a20a091da84ccf2
Nathan Harper <nathan.ed.harper@gmail.com>
2025-04-09 18:45:16 +1200

    Update to v9

--- Savvas_Rekindled_perks.lua
+++ Savvas_Rekindled_perks.lua
@@ -2,22 +2,23 @@
 perks = {
   [1]={
     remove={"(-2)"},
+    add={"(+0)"},
   },
   [2]={
     remove={"(-1)"},
-    add={"(+0) (Move the token on a (icon) backward one slot) -rolling"},
+    add={"(+0) (Move the token on a (icon) backward one slot)"},
   },
   [3]={
     remove={"(-1)"},
-    add={"(+0) (Move the token on a (icon) backward one slot) -rolling"},
+    add={"(+0) (Move the token on a (icon) backward one slot)"},
   },
   [4]={
     remove={"(-1)"},
-    add={"(+0) (Grant one ally: (move) 2)"},
+    add={"(+0) (After this attack ability, grant one ally: (move) 2)"},
   },
   [5]={
     remove={"(-1)"},
-    add={"(+0) (Grant one ally: (move) 2)"},
+    add={"(+0) (After this attack ability, grant one ally: (move) 2)"},
   },
   [6]={
     remove={"(+0)"},
@@ -36,21 +37,20 @@ perks = {
     add={"(+1) (If the target is occupying a (icon), (+2) instead)"},
   },
   [10]={
-    add={"(+2) fire"},
+    add={"(+1) muddle rolling","(+1) muddle rolling"},
   },
   [11]={
-    add={"(+2) fire"},
+    add={"((heal) 1, (target) 1 ally) rolling","((heal) 1, (target) 1 ally) rolling"},
   },
   [12]={
-    add={"(+2) fire"},
+    add={"((heal) 1, (target) 1 ally) rolling","((heal) 1, (target) 1 ally) rolling"},
   },
   [13]={
     ignore=ClassApi.PerkType.IgnoreScenario,
-    remove={"(+0)","(+0)"},
-    add={"wound rolling","wound rolling"},
+    add={"disarm rolling"},
   },
   [14]={
-    --Whenever you short rest, you may (consume) fire to move the token backward one slot on all of your active (icon)
+    --Whenever you long rest, you may destroy one (icon) within (range) 3 to place a (icon) in an empty hex within (range) 2
     add={"Perk Reminder #14"},
   },
   [15]={
@@ -60,7 +60,7 @@ perks = {
   [16]={},
   [17]={},
   [18]={
-    --When an enemy enters a hex containing a (icon), that enemy immediately suffers (damage) 1 and gains wound
+    --When an enemy enters a hex containing a (icon), that enemy immediately suffers (damage) 1, and gains wound
     add={"Perk Reminder #18"},
   },
 },

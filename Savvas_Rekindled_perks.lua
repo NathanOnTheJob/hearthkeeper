7a5426fdf0af531089e2b179534caa17c4874eaa
Nathan Harper <nathan.ed.harper@gmail.com>
2025-08-31 14:01:39 +1200

    Update to v20

--- Savvas_Rekindled_perks.lua
+++ Savvas_Rekindled_perks.lua
@@ -6,11 +6,11 @@ perks = {
   },
   [2]={
     remove={"(-1)"},
-    add={"(-1) (Place a (icon) in a hex adjacent to or occupied by the target of the attack)"},
+    add={"(+0) (Grant one ally: (move) 2 or (attack) 2)"},
   },
   [3]={
     remove={"(-1)"},
-    add={"(-1) (Place a (icon) in a hex adjacent to or occupied by the target of the attack)"},
+    add={"(+0) (Grant one ally: (move) 2 or (attack) 2)"},
   },
   [4]={
     remove={"(-1)"},
@@ -21,46 +21,45 @@ perks = {
     add={"(+0) wound"},
   },
   [6]={
-    remove={"(-1)"},
-    add={"(+0) wound"},
+    remove={"(+0)"},
+    add={"(+1 (attack) for each pair of active (icon)) rolling"},
   },
   [7]={
-    remove={"(+0)","(+0)"},
-    add={"(+0) (Add +1 (attack) for each (icon) in your active area)"},
+    remove={"(+0)"},
+    add={"(+1 (attack) for each pair of active (icon)) rolling"},
   },
   [8]={
-    remove={"(+0)","(+0)"},
-    add={"(+0) (Add +1 (attack) for each (icon) in your active area)"},
+    remove={"(+0)"},
+    add={"(+1) fire"},
   },
   [9]={
-    remove={"(+1)"},
-    add={"(+1) (After this attack ability, grant self or one of your allies: (move) 2)"},
+    remove={"(+0)"},
+    add={"(+1) fire"},
   },
   [10]={
-    remove={"(+1)"},
-    add={"(+1) (After this attack ability, grant self or one of your allies: (move) 2)"},
+    add={"(+1) (Place a (icon) in a hex adjacent to or occupied by the target of the attack)"},
   },
   [11]={
-    add={"(+1) (Gain invisible if you occupy a (icon)) rolling"},
+    add={"(+1) (Place a (icon) in a hex adjacent to or occupied by the target of the attack)"},
   },
   [12]={
-    add={"(+1) (Gain invisible if you occupy a (icon)) rolling"},
+    add={"bless (range) 3 rolling","disarm (range) 3 rolling"},
   },
   [13]={
-    add={"((heal) 1, (target) 1 ally) rolling","((heal) 1, (target) 1 ally) rolling"},
+    add={"bless (range) 3 rolling","disarm (range) 3 rolling"},
   },
   [14]={},
   [15]={
     --Once per scenario, when you would suffer damage, you may (teleport) to an unoccupied (icon) to negate the damage
     add={"Perk Reminder #15"},
   },
-  [16]={},
-  [17]={
+  [16]={
     --At the start of each scenario, you may place a (icon) in an adjacent empty hex
-    add={"Perk Reminder #17"},
+    add={"Perk Reminder #16"},
   },
+  [17]={},
   [18]={
-    --While you are occupying a hex with a (icon), all attacks targeting you gain disadvantage
+    --Immediately after each of your long rests, you may play a card from your hand for an action that places a (icon)
     add={"Perk Reminder #18"},
   },
 },
